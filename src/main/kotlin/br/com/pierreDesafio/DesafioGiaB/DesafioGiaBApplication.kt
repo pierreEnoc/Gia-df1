13e08869906b19fd52133c3484e8c9612ea3642d
Pierre enoc <pierre.enoc@gympass.com>
2021-03-23 20:59:06 -0300

    start project

---
+++ src/main/kotlin/br/com/pierreDesafio/DesafioGiaB/DesafioGiaBApplication.kt
@@ -0,0 +1,11 @@
+package br.com.pierreDesafio.DesafioGiaB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioGiaBApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioGiaBApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/pierreDesafio/DesafioGiaB/DesafioGiaBApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.pierreDesafio.DesafioGiaB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioGiaBApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
